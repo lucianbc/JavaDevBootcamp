a4fdfba6d67fc34c371c0a966eda7a3fa5266e8e
lucian.bc <lucianboaca97@gmail.com>
2018-04-19 17:30:23 +0300

    fixed access modifier problem

--- src/org/bootcamp/vechicle/Car.java
+++ src/org/bootcamp/vechicle/Car.java
@@ -3,9 +3,9 @@ package org.bootcamp.vechicle;
 public final class Car extends Vehicle {
     private String transmission = "manual";
 
-    protected Car() {}
+    public Car() {}
 
-    protected Car(int age, long numberOfMiles, boolean isDiesel, String transmission) {
+    public Car(int age, long numberOfMiles, boolean isDiesel, String transmission) {
         super(age, numberOfMiles, isDiesel);
         this.transmission = transmission;
     }
